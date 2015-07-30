addc03b421f490c89ceb370bf0898784eec02b34
boluge <boluge@gmail.com>
2015-07-30 22:50:31 +0200

    up

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,39 +2,42 @@
 <html>
     <head>
         <title>@yield('title')</title>
-        {!! Html::style('css/style.min.css') !!}
+        {!! Html::style('css/style.css') !!}
     </head>
     <body>
       <!-- Always shows a header, even in smaller screens. -->
-      <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <header class="mdl-layout__header">
-          <div class="mdl-layout__header-row">
-            <!-- Title -->
+      <div class="mdl-layout mdl-js-layout">
+        <div class="layout-transparent mdl-layout mdl-js-layout">
+          <header class="mdl-layout__header mdl-layout__header--transparent">
+            <div class="mdl-layout__header-row">
+              <!-- Title -->
+              <span class="mdl-layout-title">@yield('title')</span>
+              <!-- Add spacer, to align navigation to the right -->
+              <div class="mdl-layout-spacer"></div>
+              <!-- Navigation -->
+              <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+              </nav>
+            </div>
+          </header>
+          <div class="mdl-layout__drawer">
             <span class="mdl-layout-title">@yield('title')</span>
-            <!-- Add spacer, to align navigation to the right -->
-            <div class="mdl-layout-spacer"></div>
-            <!-- Navigation. We hide it in small screens. -->
-            <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <nav class="mdl-navigation">
               <a class="mdl-navigation__link" href="">Link</a>
               <a class="mdl-navigation__link" href="">Link</a>
               <a class="mdl-navigation__link" href="">Link</a>
               <a class="mdl-navigation__link" href="">Link</a>
             </nav>
           </div>
-        </header>
-        <div class="mdl-layout__drawer">
-          <span class="mdl-layout-title">Title</span>
-          <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="">Link</a>
-            <a class="mdl-navigation__link" href="">Link</a>
-            <a class="mdl-navigation__link" href="">Link</a>
-            <a class="mdl-navigation__link" href="">Link</a>
-          </nav>
+          <main class="mdl-layout__content">
+            @yield('header')
+          </main>
         </div>
-        <main class="mdl-layout__content">
-          <div class="page-content"><!-- Your content goes here --></div>
-        </main>
-      </div>
-      @yield('content')
+        <div class="container">
+          @yield('content')
+        </div>
     </body>
 </html>
