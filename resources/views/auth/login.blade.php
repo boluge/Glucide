48926c5cd577d69eb8ab4ecf9c13e9ea0251fa53
Stephane Deluce <boluge@gmail.com>
2015-08-05 00:23:03 +0200

    login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,6 @@
 @extends('../layouts/template')
 
 @section('content')
-        <div class="panel-heading">Login</div>
-        <div class="panel-body">
             @if (count($errors) > 0)
                 <div class="alert alert-danger">
                     <strong>Whoops!</strong> There were some problems with your input.<br><br>
@@ -13,41 +11,36 @@
                     </ul>
                 </div>
             @endif
-
-            <form class="form-horizontal" role="form" method="POST" action="{{ route('postlogin') }}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label">E-Mail Address</label>
-                    <div class="col-md-6">
-                        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                <div class="mdl-card mdl-shadow--2dp">
+                    <div class="mdl-card__title">
+                        <h2 class="mdl-card__title-text">Login</h2>
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Password</label>
-                    <div class="col-md-6">
-                        <input type="password" class="form-control" name="password">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="remember"> Remember Me
+                    <div class="mdl-card__supporting-text">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('postlogin') }}">
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="email" name="email" value="{{ old('email') }}"/>
+                                <label class="mdl-textfield__label" for="sample3">E-Mail Address</label>
+                            </div>
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+                                <input class="mdl-textfield__input" name="password" type="password"/>
+                                <label class="mdl-textfield__label" for="sample3">Password</label>
+                            </div>
+                            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-1">
+                                <input type="checkbox" class="mdl-checkbox__input" name="remember" />
+                                <span class="mdl-checkbox__label">Remember Me</span>
                             </label>
-                        </div>
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                    <div class="mdl-card__actions mdl-card--border">
+                        <a href="{{ url('/password/email') }}" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                            Get Started
+                        </a>
                     </div>
                 </div>
-
-                <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">Login</button>
                         <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+
 @endsection
